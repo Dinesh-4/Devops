5215a6cbd4e6126e76febfa67961faa5777cd081
“Dinesh-4” <dinesh46.mv@gmail.com>
2025-07-16 10:50:58 +0530

    dlafd

--- app/main.py
+++ app/main.py
@@ -180,7 +180,7 @@ def create_app():
         return jsonify({"message": "RFID received", "rfid": rfid_id})
 
     return app
- #something test yes hellovbuzz
+ #something test yes hellovbuzzdsfsdfsdf
 
 # For local running
 if __name__ == "__main__":
